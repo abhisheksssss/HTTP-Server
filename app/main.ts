e91f41e80dc26fd1a51471a6914087143323506c
abhisheksssss <abhisheksahu97521@gmail.com>
2026-01-22 00:38:13 +0530

    [any message]

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,7 @@ console.log("Logs from your program will appear here!");
 
 // TODO: Uncomment the code below to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
+  socket.on("data", () => {
     const respponse="HTTP/1.1 200 OK\r\n\r\n";
     socket.write(respponse);
     socket.end();
