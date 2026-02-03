eeeb4ddfac65318ee1e544a5b51f987e8689a0e4
abhisheksssss <abhisheksahu97521@gmail.com>
2026-02-03 22:21:48 +0530

    Implement request handling and response logic in the HTTP server

--- app/main.ts
+++ app/main.ts
@@ -5,10 +5,31 @@ console.log("Logs from your program will appear here!");
 
 // TODO: Uncomment the code below to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("data", () => {
-    const respponse="HTTP/1.1 200 OK\r\n\r\n";
-    socket.write(respponse);
-    socket.end();
+  socket.on("data", (data) => {
+const request=data.toString();
+
+console.log(request);
+
+const requestLine=request.split("\r\n")[0];
+const parts=requestLine.split(' ');
+
+if(parts.length>=2){
+const path=parts[1];
+
+if(path=="/"){
+    const response = "HTTP/1.1 200 OK\r\n\r\n";
+    socket.write(response);
+}else{
+  const response="HTTP/1.1 404 NOt found\r\n\r\n";
+  socket.write(response);
+}
+}else{
+    const response = "HTTP/1.1 400 Bad Request\r\n\r\n";
+      socket.write(response);
+}
+
+  socket.end();
+
   });
 });
 
